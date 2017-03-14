9be1a33b69dafaf5b3909fa266e244da35974467
Tama <tama2@wp.pl>
2017-03-14 20:42:25 +0100

    Added panel for Worker

--- Stacja_paliw/Stacja_paliw/Controllers/WorkerPanelController.cs
+++ Stacja_paliw/Stacja_paliw/Controllers/WorkerPanelController.cs
@@ -6,8 +6,9 @@ using System.Web.Mvc;
 
 namespace Stacja_paliw.Controllers
 {
-    public class HomeController : Controller
+    public class WorkerPanelController : Controller
     {
+        // GET: Worker
         public ActionResult Index()
         {
             return View();
