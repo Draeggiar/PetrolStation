18db864e9723ab6af6d649e6b81ae8fa93813b04
Tama <tama2@wp.pl>
2017-03-15 15:23:28 +0100

    Working directory cleanup

---
+++ Stacja_paliw/Stacja_paliw/Areas/Worker/Controllers/MonitoringController.cs
@@ -0,0 +1,37 @@
+﻿using System.Web.Mvc;
+using MonitoringCompany;
+
+namespace Stacja_paliw.Areas.Worker.Controllers
+{
+    public class MonitoringController : Controller
+    {
+        private MonitoringCompany.MonitoringService _service = new MonitoringService();
+        // GET: Monitoring
+        public ActionResult Index()
+        {
+            ViewBag.ServiceStatus = _service.GetServiceStatus().MonitoringStarted.ToString().ToLower();
+            return View();
+        }
+
+        public ActionResult View(int cameraID)
+        {
+            ViewBag.Message = _service.ViewCamera(cameraID);
+            ViewBag.ServiceStatus = _service.GetServiceStatus().MonitoringStarted.ToString().ToLower();
+            return View("Index");
+        }
+
+        public ActionResult Start()
+        {
+            ViewBag.Message = _service.StartMonitoring();
+            ViewBag.ServiceStatus = _service.GetServiceStatus().MonitoringStarted.ToString().ToLower();           
+            return View("Index");
+        }
+
+        public ActionResult Stop()
+        {
+            ViewBag.Message = _service.StopMonitoring();
+            ViewBag.ServiceStatus = _service.GetServiceStatus().MonitoringStarted.ToString().ToLower();
+            return View("Index");
+        }
+    }
+}
