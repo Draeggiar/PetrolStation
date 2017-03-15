e14057948f5e3a33b5613f896236a2fc564317e7
Tama <tama2@wp.pl>
2017-03-15 15:23:28 +0100

    Working directory cleanup

---
+++ Stacja_paliw/Stacja_paliw/Areas/Worker/Controllers/WorkerPanelController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace Stacja_paliw.Areas.Worker.Controllers
+{
+    public class WorkerPanelController : Controller
+    {
+        // GET: Worker
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
