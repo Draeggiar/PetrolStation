e91eb50dbcdefeff218723ea2ce3a0bb0c9e9c97
slamnation <slamnation01@gmail.com>
2017-04-24 09:46:24 +0200

    User specific main page + bootstrap 4.0.0

--- Stacja_paliw/Stacja_paliw/Controllers/HomeController.cs
+++ Stacja_paliw/Stacja_paliw/Controllers/HomeController.cs
@@ -1,12 +1,18 @@
-﻿using System.Web.Mvc;
+﻿using PetrolStationDB;
+using System.Linq;
+using System.Web.Mvc;
 
 namespace Stacja_paliw.Controllers
 {
     public class HomeController : Controller
     {
+        private PSDbContext db = new PSDbContext();
+
         public ActionResult Index()
         {
-            return View();
+            var items = from p in db.Prices select p;
+
+            return View(db.Prices.ToList().Last());
         }
     }
 }
