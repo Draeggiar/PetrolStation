ae07b319d88f56de5725ae0ab35d941995aa87c8
slamnation <slamnation01@gmail.com>
2017-04-27 19:20:58 +0200

    Fixed migration and references

---
+++ Stacja_paliw/Stacja_paliw/Migrations/201704271705487_fix.cs
@@ -0,0 +1,16 @@
+namespace Stacja_paliw.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class fix : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
